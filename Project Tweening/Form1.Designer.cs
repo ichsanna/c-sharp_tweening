89450e311cf363fbc6a6b2d657a768a86ec9222b
ichsanna <43778566+ichsanna@users.noreply.github.com>
2019-03-02 10:26:18 +0700

    First commit

---
+++ Project Tweening/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListView();
+            this.mouseStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button_animate = new System.Windows.Forms.Button();
+            this.checkBox_drawmode = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Location = new System.Drawing.Point(859, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 396);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.UseCompatibleStateImageBehavior = false;
+            // 
+            // mouseStatus
+            // 
+            this.mouseStatus.AutoSize = true;
+            this.mouseStatus.Location = new System.Drawing.Point(12, 539);
+            this.mouseStatus.Name = "mouseStatus";
+            this.mouseStatus.Size = new System.Drawing.Size(46, 17);
+            this.mouseStatus.TabIndex = 2;
+            this.mouseStatus.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Drawing 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Drawing 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button_animate
+            // 
+            this.button_animate.Location = new System.Drawing.Point(474, 12);
+            this.button_animate.Name = "button_animate";
+            this.button_animate.Size = new System.Drawing.Size(118, 31);
+            this.button_animate.TabIndex = 6;
+            this.button_animate.Text = "Animate!";
+            this.button_animate.UseVisualStyleBackColor = true;
+            this.button_animate.Click += new System.EventHandler(this.button_animate_Click);
+            // 
+            // checkBox_drawmode
+            // 
+            this.checkBox_drawmode.AutoSize = true;
+            this.checkBox_drawmode.Checked = true;
+            this.checkBox_drawmode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_drawmode.Location = new System.Drawing.Point(12, 18);
+            this.checkBox_drawmode.Name = "checkBox_drawmode";
+            this.checkBox_drawmode.Size = new System.Drawing.Size(101, 21);
+            this.checkBox_drawmode.TabIndex = 7;
+            this.checkBox_drawmode.Text = "Draw Mode";
+            this.checkBox_drawmode.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1080, 565);
+            this.Controls.Add(this.checkBox_drawmode);
+            this.Controls.Add(this.button_animate);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.mouseStatus);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listBox1;
+        private System.Windows.Forms.Label mouseStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_animate;
+        private System.Windows.Forms.CheckBox checkBox_drawmode;
+    }
+}
+
---
+++ Project Tweening/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+		Graphics g; Pen p; PointF cursor; Pen line; Pen erase;
+        PointF[] points = new PointF[100];
+        PointF[] points2 = new PointF[100];
+        int maxpoint = 0;
+        int maxpoint2 = 0;
+        int drawmode = 1;
+        int current = 1;
+        float[] x = new float[50];
+        float[] y = new float[50];
+        /*
+            points[0] = {
+             */
+        public Form1()
+        {
+            InitializeComponent();
+			g = this.CreateGraphics();
+            erase = new Pen(Color.White, 3);
+			p = new Pen(Color.Black, 3);
+            line = new Pen(Color.CadetBlue, 3);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+			cursor = this.PointToClient(Cursor.Position);
+            mouseStatus.Text = "X: " + cursor.X + " Y: " + cursor.Y + " Current:" +current;
+            //mouseStatus.Text = "X: " + points.Length + " Y: " + points2.Length;
+            //if (k>0) mouseStatus.Text = points[k-1].X.ToString();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (checkBox_drawmode.Checked)
+            {
+                listBox1.Items.Add("X: " + cursor.X + " Y: " + cursor.Y);
+                g.DrawEllipse(p, cursor.X - 1, cursor.Y - 1, 2, 2);
+                if (current == 1) points[maxpoint++] = new PointF(cursor.X, cursor.Y);
+                else if (current == 2) points2[maxpoint2++] = new PointF(cursor.X, cursor.Y);
+
+                if ((current == 1 && maxpoint > 1) || (current == 2 && maxpoint2 > 1))
+                {
+                    if (current == 1) g.DrawLine(line, points[maxpoint - 2].X, points[maxpoint - 2].Y, points[maxpoint - 1].X, points[maxpoint - 1].Y);
+                    else if (current == 2) g.DrawLine(line, points2[maxpoint2 - 2].X, points2[maxpoint2 - 2].Y, points2[maxpoint2 - 1].X, points2[maxpoint2 - 1].Y);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            current = 1;
+            draw();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            current = 2;
+            draw();
+        }
+        private void draw()
+        {
+            g.Clear(Color.White);
+            if (current == 1)
+            {
+                for (int i = 0; i < maxpoint; i++)
+                {
+                    g.DrawEllipse(p, points[i].X, points[i].Y, 2, 2);
+                    if (i > 0) g.DrawLine(line, points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
+                }
+            }
+            if (current == 2)
+            {
+                for (int i = 0; i < maxpoint2; i++)
+                {
+                    g.DrawEllipse(p, points2[i].X, points2[i].Y, 2, 2);
+                    if (i > 0) g.DrawLine(line, points2[i - 1].X, points2[i - 1].Y, points2[i].X, points2[i].Y);
+                }
+            }
+        }
+        private void animate()
+        {
+            // optimal a <= 5000
+            for (int a = 1; a <= 2000; a++)
+            {
+                Console.Write("haha");
+            }
+            g.Clear(Color.White);
+            for (int i = 0; i < maxpoint; i++)
+            {
+                g.DrawEllipse(p, points[i].X, points[i].Y, 2, 2);
+                if (i > 0) g.DrawLine(line, points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
+            }
+        }
+        private void button_animate_Click(object sender, EventArgs e)
+        {
+            int frame = 90;
+            for (int z = 0; z <= 2; z++)
+            {
+                for (int k = 0; k < maxpoint; k++)
+                {
+                    x[k] = points[k].X - points2[k].X;
+                    y[k] = points[k].Y - points2[k].Y;
+                }
+                for (int l = 0; l < frame; l++)
+                {
+                    for (int m = 0; m < maxpoint; m++)
+                    {
+                        points[m].X = points[m].X - (x[m] / frame);
+                        points[m].Y = points[m].Y - (y[m] / frame);
+                    }
+                    animate();
+                }
+                for (int l = 0; l < frame; l++)
+                {
+                    for (int m = maxpoint - 1; m >= 0; m--)
+                    {
+                        points[m].X = points[m].X + (x[m] / frame);
+                        points[m].Y = points[m].Y + (y[m] / frame);
+                    }
+                    animate();
+                }
+            }
+        }
+        private void pb_Bezier_Paint(object sender, PaintEventArgs e)
+        {
+            Point P1 = new Point(10, 300);
+            Point P2 = new Point(180, 50);
+            Point P3 = new Point(320, 300);
+
+            ZeichneBezier(6, P1, P2, P3, e, true);
+        }
+
+        private void ZeichneBezier(int n, Point P1, Point P2, Point P3, PaintEventArgs pva, bool initial)
+        {
+            Graphics g = pva.Graphics;
+            Pen bkStift = new Pen(Color.Red, 2);
+            Pen kpStift = new Pen(Color.Black, 3);
+
+            if (initial)
+            {
+                g.DrawLine(kpStift, P1, P2);
+                g.DrawLine(kpStift, P2, P3);
+            }
+
+            if (n > 0)
+            {
+                Point P12 = new Point((P1.X + P2.X) / 2, (P1.Y + P2.Y) / 2);
+                Point P23 = new Point((P2.X + P3.X) / 2, (P2.Y + P3.Y) / 2);
+                Point P123 = new Point((P12.X + P23.X) / 2, (P12.Y + P23.Y) / 2);
+
+                ZeichneBezier(n - 1, P1, P12, P123, pva, false);
+                ZeichneBezier(n - 1, P123, P23, P3, pva, false);
+            }
+            else
+            {
+                g.DrawLine(bkStift, P1, P2);
+                g.DrawLine(bkStift, P2, P3);
+            }
+        }
+    }
+}

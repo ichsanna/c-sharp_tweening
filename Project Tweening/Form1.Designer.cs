c65150a1842bb76aac6f729bf074db4bdc4756e3
ichsanna <43778566+ichsanna@users.noreply.github.com>
2019-03-03 09:21:43 +0700

    Update

--- Project Tweening/Form1.Designer.cs
+++ Project Tweening/Form1.Designer.cs
@@ -33,21 +33,28 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button_animate = new System.Windows.Forms.Button();
-            this.checkBox_drawmode = new System.Windows.Forms.CheckBox();
+            this.pb_canvas = new System.Windows.Forms.PictureBox();
+            this.button_makecurve = new System.Windows.Forms.Button();
+            this.button_reset = new System.Windows.Forms.Button();
+            this.button_join = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.draw_whale = new System.Windows.Forms.Button();
+            this.draw_elephant = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.Location = new System.Drawing.Point(859, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(209, 396);
+            this.listBox1.Size = new System.Drawing.Size(209, 524);
             this.listBox1.TabIndex = 1;
             this.listBox1.UseCompatibleStateImageBehavior = false;
             // 
             // mouseStatus
             // 
             this.mouseStatus.AutoSize = true;
-            this.mouseStatus.Location = new System.Drawing.Point(12, 539);
+            this.mouseStatus.Location = new System.Drawing.Point(22, 542);
             this.mouseStatus.Name = "mouseStatus";
             this.mouseStatus.Size = new System.Drawing.Size(46, 17);
             this.mouseStatus.TabIndex = 2;
@@ -55,53 +62,117 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(176, 12);
+            this.button1.Location = new System.Drawing.Point(15, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 31);
+            this.button1.Size = new System.Drawing.Size(77, 31);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Drawing 1";
+            this.button1.Text = "Layer 1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(323, 12);
+            this.button2.Location = new System.Drawing.Point(98, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 31);
+            this.button2.Size = new System.Drawing.Size(78, 31);
             this.button2.TabIndex = 5;
-            this.button2.Text = "Drawing 2";
+            this.button2.Text = "Layer 2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button_animate
             // 
-            this.button_animate.Location = new System.Drawing.Point(474, 12);
+            this.button_animate.Location = new System.Drawing.Point(683, 12);
             this.button_animate.Name = "button_animate";
-            this.button_animate.Size = new System.Drawing.Size(118, 31);
+            this.button_animate.Size = new System.Drawing.Size(82, 31);
             this.button_animate.TabIndex = 6;
             this.button_animate.Text = "Animate!";
             this.button_animate.UseVisualStyleBackColor = true;
             this.button_animate.Click += new System.EventHandler(this.button_animate_Click);
             // 
-            // checkBox_drawmode
+            // pb_canvas
             // 
-            this.checkBox_drawmode.AutoSize = true;
-            this.checkBox_drawmode.Checked = true;
-            this.checkBox_drawmode.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox_drawmode.Location = new System.Drawing.Point(12, 18);
-            this.checkBox_drawmode.Name = "checkBox_drawmode";
-            this.checkBox_drawmode.Size = new System.Drawing.Size(101, 21);
-            this.checkBox_drawmode.TabIndex = 7;
-            this.checkBox_drawmode.Text = "Draw Mode";
-            this.checkBox_drawmode.UseVisualStyleBackColor = true;
+            this.pb_canvas.BackColor = System.Drawing.Color.White;
+            this.pb_canvas.Location = new System.Drawing.Point(15, 52);
+            this.pb_canvas.Name = "pb_canvas";
+            this.pb_canvas.Size = new System.Drawing.Size(841, 487);
+            this.pb_canvas.TabIndex = 8;
+            this.pb_canvas.TabStop = false;
+            this.pb_canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_canvas_MouseClick);
+            this.pb_canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pb_canvas_MouseMove);
+            // 
+            // button_makecurve
+            // 
+            this.button_makecurve.Location = new System.Drawing.Point(209, 12);
+            this.button_makecurve.Name = "button_makecurve";
+            this.button_makecurve.Size = new System.Drawing.Size(112, 31);
+            this.button_makecurve.TabIndex = 9;
+            this.button_makecurve.Text = "Make Curve";
+            this.button_makecurve.UseVisualStyleBackColor = true;
+            this.button_makecurve.Click += new System.EventHandler(this.button_makecurve_Click);
+            // 
+            // button_reset
+            // 
+            this.button_reset.Location = new System.Drawing.Point(771, 12);
+            this.button_reset.Name = "button_reset";
+            this.button_reset.Size = new System.Drawing.Size(82, 31);
+            this.button_reset.TabIndex = 10;
+            this.button_reset.Text = "Reset";
+            this.button_reset.UseVisualStyleBackColor = true;
+            this.button_reset.Click += new System.EventHandler(this.button_reset_Click);
+            // 
+            // button_join
+            // 
+            this.button_join.Location = new System.Drawing.Point(327, 12);
+            this.button_join.Name = "button_join";
+            this.button_join.Size = new System.Drawing.Size(112, 31);
+            this.button_join.TabIndex = 11;
+            this.button_join.Text = "Join Line";
+            this.button_join.UseVisualStyleBackColor = true;
+            this.button_join.Click += new System.EventHandler(this.button_join_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 542);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // draw_whale
+            // 
+            this.draw_whale.Location = new System.Drawing.Point(464, 12);
+            this.draw_whale.Name = "draw_whale";
+            this.draw_whale.Size = new System.Drawing.Size(96, 31);
+            this.draw_whale.TabIndex = 13;
+            this.draw_whale.Text = "Whale";
+            this.draw_whale.UseVisualStyleBackColor = true;
+            this.draw_whale.Click += new System.EventHandler(this.draw_whale_Click);
+            // 
+            // draw_elephant
+            // 
+            this.draw_elephant.Location = new System.Drawing.Point(566, 12);
+            this.draw_elephant.Name = "draw_elephant";
+            this.draw_elephant.Size = new System.Drawing.Size(96, 31);
+            this.draw_elephant.TabIndex = 14;
+            this.draw_elephant.Text = "Elephant";
+            this.draw_elephant.UseVisualStyleBackColor = true;
+            this.draw_elephant.Click += new System.EventHandler(this.draw_elephant_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1080, 565);
-            this.Controls.Add(this.checkBox_drawmode);
+            this.Controls.Add(this.draw_elephant);
+            this.Controls.Add(this.draw_whale);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_join);
+            this.Controls.Add(this.button_reset);
+            this.Controls.Add(this.button_makecurve);
+            this.Controls.Add(this.pb_canvas);
             this.Controls.Add(this.button_animate);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -109,8 +180,7 @@
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_canvas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,7 +192,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button_animate;
-        private System.Windows.Forms.CheckBox checkBox_drawmode;
+        private System.Windows.Forms.PictureBox pb_canvas;
+        private System.Windows.Forms.Button button_makecurve;
+        private System.Windows.Forms.Button button_reset;
+        private System.Windows.Forms.Button button_join;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button draw_whale;
+        private System.Windows.Forms.Button draw_elephant;
     }
 }
 
--- Project Tweening/Form1.cs
+++ Project Tweening/Form1.cs
@@ -12,50 +12,26 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-		Graphics g; Pen p; PointF cursor; Pen line; Pen erase;
+        Graphics g;
+        Pen p = new Pen(Color.Black, 3);
+        PointF cursor;
+        Pen line = new Pen(Color.CadetBlue, 3);
+        Pen erase = new Pen(Color.White, 3);
         PointF[] points = new PointF[100];
         PointF[] points2 = new PointF[100];
         int maxpoint = 0;
         int maxpoint2 = 0;
-        int drawmode = 1;
         int current = 1;
-        float[] x = new float[50];
-        float[] y = new float[50];
-        /*
-            points[0] = {
-             */
+        int join = 0;
+        int join2 = 0;
+        int[] curvepos = new int[100];
+        int[] curvepos2 = new int[100];
+        float[] x = new float[100];
+        float[] y = new float[100];
         public Form1()
         {
             InitializeComponent();
-			g = this.CreateGraphics();
-            erase = new Pen(Color.White, 3);
-			p = new Pen(Color.Black, 3);
-            line = new Pen(Color.CadetBlue, 3);
-        }
-
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
-        {
-			cursor = this.PointToClient(Cursor.Position);
-            mouseStatus.Text = "X: " + cursor.X + " Y: " + cursor.Y + " Current:" +current;
-            //mouseStatus.Text = "X: " + points.Length + " Y: " + points2.Length;
-            //if (k>0) mouseStatus.Text = points[k-1].X.ToString();
-        }
-
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
-        {
-            if (checkBox_drawmode.Checked)
-            {
-                listBox1.Items.Add("X: " + cursor.X + " Y: " + cursor.Y);
-                g.DrawEllipse(p, cursor.X - 1, cursor.Y - 1, 2, 2);
-                if (current == 1) points[maxpoint++] = new PointF(cursor.X, cursor.Y);
-                else if (current == 2) points2[maxpoint2++] = new PointF(cursor.X, cursor.Y);
-
-                if ((current == 1 && maxpoint > 1) || (current == 2 && maxpoint2 > 1))
-                {
-                    if (current == 1) g.DrawLine(line, points[maxpoint - 2].X, points[maxpoint - 2].Y, points[maxpoint - 1].X, points[maxpoint - 1].Y);
-                    else if (current == 2) g.DrawLine(line, points2[maxpoint2 - 2].X, points2[maxpoint2 - 2].Y, points2[maxpoint2 - 1].X, points2[maxpoint2 - 1].Y);
-                }
-            }
+            g = pb_canvas.CreateGraphics();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -76,36 +52,64 @@ namespace WindowsFormsApp1
             {
                 for (int i = 0; i < maxpoint; i++)
                 {
+                    if (curvepos[i] == 1)
+                    {
+                        i++;
+                        makecurve(i+1);
+                    }
+                    else
+                    {
+                        if (i > 0) g.DrawLine(line, points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
+                    }
                     g.DrawEllipse(p, points[i].X, points[i].Y, 2, 2);
-                    if (i > 0) g.DrawLine(line, points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
                 }
+                if (join==1) g.DrawLine(line, points[maxpoint - 1].X, points[maxpoint - 1].Y, points[0].X, points[0].Y);
             }
             if (current == 2)
             {
                 for (int i = 0; i < maxpoint2; i++)
                 {
+                    if (curvepos2[i] == 1)
+                    {
+                        i++;
+                        makecurve(i+1);
+                    }
+                    else
+                    {
+                        if (i > 0) g.DrawLine(line, points2[i - 1].X, points2[i - 1].Y, points2[i].X, points2[i].Y);
+                    }
                     g.DrawEllipse(p, points2[i].X, points2[i].Y, 2, 2);
-                    if (i > 0) g.DrawLine(line, points2[i - 1].X, points2[i - 1].Y, points2[i].X, points2[i].Y);
                 }
+                if (join2==1) g.DrawLine(line, points2[maxpoint2 - 1].X, points2[maxpoint2 - 1].Y, points2[0].X, points2[0].Y);
             }
         }
         private void animate()
         {
-            // optimal a <= 5000
-            for (int a = 1; a <= 2000; a++)
+            // optimal a <= 4000
+            for (int a = 1; a <= 50000; a++)
             {
                 Console.Write("haha");
             }
             g.Clear(Color.White);
             for (int i = 0; i < maxpoint; i++)
             {
+                if (curvepos[i] == 1)
+                {
+                    i++;
+                    makecurve(i + 1);
+                }
+                else
+                {
+                    if (i > 0) g.DrawLine(line, points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
+                }
                 g.DrawEllipse(p, points[i].X, points[i].Y, 2, 2);
-                if (i > 0) g.DrawLine(line, points[i - 1].X, points[i - 1].Y, points[i].X, points[i].Y);
             }
+            if (join == 1) g.DrawLine(line, points[maxpoint - 1].X, points[maxpoint - 1].Y, points[0].X, points[0].Y);
         }
         private void button_animate_Click(object sender, EventArgs e)
         {
-            int frame = 90;
+            current = 1;
+            int frame = 60;
             for (int z = 0; z <= 2; z++)
             {
                 for (int k = 0; k < maxpoint; k++)
@@ -133,41 +137,211 @@ namespace WindowsFormsApp1
                 }
             }
         }
-        private void pb_Bezier_Paint(object sender, PaintEventArgs e)
-        {
-            Point P1 = new Point(10, 300);
-            Point P2 = new Point(180, 50);
-            Point P3 = new Point(320, 300);
 
-            ZeichneBezier(6, P1, P2, P3, e, true);
+        private void pb_canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            cursor = pb_canvas.PointToClient(Cursor.Position);
+            mouseStatus.Text = "X: " + cursor.X + " Y: " + cursor.Y + " Current:" + current;
         }
 
-        private void ZeichneBezier(int n, Point P1, Point P2, Point P3, PaintEventArgs pva, bool initial)
+        private void pb_canvas_MouseClick(object sender, MouseEventArgs e)
         {
-            Graphics g = pva.Graphics;
-            Pen bkStift = new Pen(Color.Red, 2);
-            Pen kpStift = new Pen(Color.Black, 3);
+            listBox1.Items.Add("X: " + cursor.X + " Y: " + cursor.Y);
+            g.DrawEllipse(p, cursor.X - 1, cursor.Y - 1, 2, 2);
+            if (current == 1) points[maxpoint++] = new PointF(cursor.X, cursor.Y);
+            else if (current == 2) points2[maxpoint2++] = new PointF(cursor.X, cursor.Y);
 
-            if (initial)
+            if ((current == 1 && maxpoint > 1) || (current == 2 && maxpoint2 > 1))
             {
-                g.DrawLine(kpStift, P1, P2);
-                g.DrawLine(kpStift, P2, P3);
+                if (current == 1) g.DrawLine(line, points[maxpoint - 2].X, points[maxpoint - 2].Y, points[maxpoint - 1].X, points[maxpoint - 1].Y);
+                else if (current == 2) g.DrawLine(line, points2[maxpoint2 - 2].X, points2[maxpoint2 - 2].Y, points2[maxpoint2 - 1].X, points2[maxpoint2 - 1].Y);
+            }
+        }
+
+        private void button_makecurve_Click(object sender, EventArgs e)
+        {
+            makecurve(maxpoint);
+        }
+        private void makecurve(int currentpoint)
+        {
+            PointF P1 = new PointF();
+            PointF P2 = new PointF();
+            PointF P3 = new PointF();
+            if (current == 1)
+            {
+                P1 = points[currentpoint - 3];
+                P2 = points[currentpoint - 2];
+                P3 = points[currentpoint - 1];
+                curvepos[currentpoint - 2] = 1;
+            }
+            else if (current == 2)
+            {
+                P1 = points2[currentpoint - 3];
+                P2 = points2[currentpoint - 2];
+                P3 = points2[currentpoint - 1];
+                curvepos2[currentpoint - 2] = 1;
             }
 
-            if (n > 0)
-            {
-                Point P12 = new Point((P1.X + P2.X) / 2, (P1.Y + P2.Y) / 2);
-                Point P23 = new Point((P2.X + P3.X) / 2, (P2.Y + P3.Y) / 2);
-                Point P123 = new Point((P12.X + P23.X) / 2, (P12.Y + P23.Y) / 2);
+            g.DrawBezier(line, P1, P2, P3, P3);
+        }
 
-                ZeichneBezier(n - 1, P1, P12, P123, pva, false);
-                ZeichneBezier(n - 1, P123, P23, P3, pva, false);
-            }
-            else
+        private void button_reset_Click(object sender, EventArgs e)
+        {
+            join = 0;
+            join2 = 0;
+            maxpoint = 0;
+            maxpoint2 = 0;
+            pb_canvas.Invalidate();
+        }
+
+        private void button_join_Click(object sender, EventArgs e)
+        {
+            if (current == 1)
             {
-                g.DrawLine(bkStift, P1, P2);
-                g.DrawLine(bkStift, P2, P3);
+                if (join == 1)
+                {
+                    join = 0;
+                }
+                else
+                {
+                    join = 1;
+                    g.DrawLine(line, points[maxpoint - 1].X, points[maxpoint - 1].Y, points[0].X, points[0].Y);
+                }
             }
+            else if (current == 2)
+            {
+                if (join2 == 1)
+                {
+                    join2 = 0;
+                }
+                else
+                {
+                    join2 = 1;
+                    g.DrawLine(line, points2[maxpoint2 - 1].X, points2[maxpoint2 - 1].Y, points2[0].X, points2[0].Y);
+                }
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.ScaleTransform(1.0f, -1.0f);
+            e.Graphics.TranslateTransform(0, -this.ClientRectangle.Height);
+            e.Graphics.DrawLine(Pens.Black, 0, 0, 100, 100);
+        }
+        private void draw_whale_Click(object sender, EventArgs e)
+        {
+            maxpoint2 = 36;
+            points2[0].X = 137; points2[0].Y = 400 - 267;
+            points2[1].X = 96; points2[1].Y = 400 - 227;
+            points2[2].X = 96; points2[2].Y = 400 - 192;
+            points2[3].X = 98; points2[3].Y = 400 - 161;
+            points2[4].X = 106; points2[4].Y = 400 - 120;
+            points2[5].X = 177; points2[5].Y = 400 - 102;
+            points2[6].X = 290; points2[6].Y = 400 - 104;
+            points2[7].X = 378; points2[7].Y = 400 - 106;
+            points2[8].X = 426; points2[8].Y = 400 - 124;
+            points2[9].X = 450; points2[9].Y = 400 - 187;
+            points2[10].X = 453; points2[10].Y = 400 - 208;
+            points2[11].X = 464; points2[11].Y = 400 - 208;
+            points2[12].X = 481; points2[12].Y = 400 - 211;
+            points2[13].X = 505; points2[13].Y = 400 - 242;
+            points2[14].X = 513; points2[14].Y = 400 - 270;
+            points2[15].X = 496; points2[15].Y = 400 - 271;
+            points2[16].X = 472; points2[16].Y = 400 - 271;
+            points2[17].X = 443; points2[17].Y = 400 - 271;
+            points2[18].X = 438; points2[18].Y = 400 - 268;
+            points2[19].X = 431; points2[19].Y = 400 - 272;
+            points2[20].X = 421; points2[20].Y = 400 - 277;
+            points2[21].X = 399; points2[21].Y = 400 - 275;
+            points2[22].X = 388; points2[22].Y = 400 - 275;
+            points2[23].X = 373; points2[23].Y = 400 - 275;
+            points2[24].X = 370; points2[24].Y = 400 - 259;
+            points2[25].X = 377; points2[25].Y = 400 - 228;
+            points2[26].X = 403; points2[26].Y = 400 - 216;
+            points2[27].X = 413; points2[27].Y = 400 - 216;
+            points2[28].X = 413; points2[28].Y = 400 - 211;
+            points2[29].X = 395; points2[29].Y = 400 - 186;
+            points2[30].X = 360; points2[30].Y = 400 - 184;
+            points2[31].X = 327; points2[31].Y = 400 - 220;
+            points2[32].X = 292; points2[32].Y = 400 - 255;
+            points2[33].X = 231; points2[33].Y = 400 - 265;
+            points2[34].X = 189; points2[34].Y = 400 - 267;
+            points2[35].X = 163; points2[35].Y = 400 - 269;
+            join2 = 1;
+        }
+
+        private void draw_elephant_Click(object sender, EventArgs e)
+        {
+            maxpoint = 66;
+            points[0].X = 126; points[0].Y = 400 - 209;
+            points[1].X = 125; points[0].Y = 400 - 193;
+            points[1].X = 121; points[1].Y = 400 - 183;
+            points[2].X = 118; points[2].Y = 400 - 165;
+            points[3].X = 124; points[3].Y = 400 - 131;
+            points[4].X = 131; points[4].Y = 400 - 141;
+            points[5].X = 136; points[5].Y = 400 - 184;
+            points[6].X = 135; points[6].Y = 400 - 208;
+            points[7].X = 144; points[7].Y = 400 - 239;
+            points[8].X = 145; points[8].Y = 400 - 241;
+            points[9].X = 144; points[9].Y = 400 - 235;
+            points[10].X = 145; points[10].Y = 400 - 185;
+            points[11].X = 145; points[11].Y = 400 - 141;
+            points[12].X = 129; points[12].Y = 400 - 86;
+            points[13].X = 125; points[13].Y = 400 - 70;
+            points[14].X = 127; points[14].Y = 400 - 63;
+            points[15].X = 146; points[15].Y = 400 - 57;
+            points[16].X = 173; points[16].Y = 400 - 64;
+            points[17].X = 177; points[17].Y = 400 - 79;
+            points[18].X = 189; points[18].Y = 400 - 128;
+            points[19].X = 204; points[19].Y = 400 - 146;
+            points[20].X = 213; points[20].Y = 400 - 146;
+            points[21].X = 246; points[21].Y = 400 - 135;
+            points[22].X = 286; points[22].Y = 400 - 142;
+            points[23].X = 306; points[23].Y = 400 - 147;
+            points[24].X = 313; points[24].Y = 400 - 144;
+            points[25].X = 326; points[25].Y = 400 - 99;
+            points[26].X = 338; points[26].Y = 400 - 64;
+            points[27].X = 362; points[27].Y = 400 - 57;
+            points[28].X = 376; points[28].Y = 400 - 59;
+            points[29].X = 381; points[29].Y = 400 - 66;
+            points[30].X = 375; points[30].Y = 400 - 91;
+            points[31].X = 358; points[31].Y = 400 - 144;
+            points[32].X = 363; points[32].Y = 400 - 181;
+            points[33].X = 368; points[33].Y = 400 - 185;
+            points[34].X = 381; points[34].Y = 400 - 175;
+            points[35].X = 390; points[35].Y = 400 - 159;
+            points[36].X = 419; points[36].Y = 400 - 123;
+            points[37].X = 457; points[37].Y = 400 - 122;
+            points[38].X = 486; points[38].Y = 400 - 118;
+            points[39].X = 509; points[39].Y = 400 - 141;
+            points[40].X = 514; points[40].Y = 400 - 158;
+            points[41].X = 508; points[41].Y = 400 - 160;
+            points[42].X = 494; points[42].Y = 400 - 160;
+            points[43].X = 487; points[43].Y = 400 - 161;
+            points[44].X = 484; points[44].Y = 400 - 157;
+            points[45].X = 479; points[45].Y = 400 - 138;
+            points[46].X = 473; points[46].Y = 400 - 137;
+            points[47].X = 460; points[47].Y = 400 - 134;
+            points[48].X = 442; points[48].Y = 400 - 140;
+            points[49].X = 429; points[49].Y = 400 - 168;
+            points[50].X = 435; points[50].Y = 400 - 202;
+            points[51].X = 441; points[51].Y = 400 - 236;
+            points[52].X = 430; points[52].Y = 400 - 270;
+            points[53].X = 415; points[53].Y = 400 - 288;
+            points[54].X = 402; points[54].Y = 400 - 292;
+            points[55].X = 391; points[55].Y = 400 - 296;
+            points[56].X = 381; points[56].Y = 400 - 308;
+            points[57].X = 359; points[57].Y = 400 - 317;
+            points[58].X = 346; points[58].Y = 400 - 316;
+            points[59].X = 322; points[59].Y = 400 - 300;
+            points[60].X = 320; points[60].Y = 400 - 219;
+            points[61].X = 320; points[61].Y = 400 - 285;
+            points[62].X = 302; points[62].Y = 400 - 288;
+            points[63].X = 244; points[63].Y = 400 - 297;
+            points[64].X = 185; points[64].Y = 400 - 286;
+            points[65].X = 131; points[65].Y = 400 - 240;
+            join = 1;
         }
     }
 }
